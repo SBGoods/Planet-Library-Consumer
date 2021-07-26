18aacd4a3e8d9333c6f9d2dd5313860b0ebbd7ea
SBGoods <selena_goods@yahoo.com>
2021-07-26 14:40:53 -0400

    integrate planet library into main app

--- app/src/main/java/com/goods/android/planet_library_consumer/MainActivity.kt
+++ app/src/main/java/com/goods/android/planet_library_consumer/MainActivity.kt
@@ -2,10 +2,19 @@ package com.goods.android.planet_library_consumer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.goods.android.mylibrary.PlanetFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<PlanetFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
