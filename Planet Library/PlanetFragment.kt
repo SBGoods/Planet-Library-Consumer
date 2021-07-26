1b466e02491d6aa42265ba1d5d7f3243c9f323bc
SBGoods <selena_goods@yahoo.com>
2021-07-26 14:38:30 -0400

    add planet library

---
+++ Planet Library/PlanetFragment.kt
@@ -0,0 +1,30 @@
+package com.goods.android.mylibrary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.goods.android.mylibrary.model.Planet
+
+class PlanetFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_planet, container, false)
+        val planetNameTextView = view.findViewById<TextView>(R.id.planetNameTextView)
+        val planetImageView = view.findViewById<ImageView>(R.id.planetImageView)
+
+        planetImageView.setImageResource(R.drawable.mercury)
+        planetNameTextView.setText(R.string.mercury_planet_name)
+
+
+        return view
+    }
+
+}
